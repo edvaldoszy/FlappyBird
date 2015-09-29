da038b553be9d2db50a28c00b0858b2bc586b0ee
Edvaldo Szymonek <edvaldoszy@gmail.com>
2015-09-28 21:10:02 -0300

    Arquivos do projeto e configurações iniciais

---
+++ FlappyBird/desktop/src/com/edvaldotsi/flappybird/desktop/DesktopLauncher.java
@@ -0,0 +1,14 @@
+package com.edvaldotsi.flappybird.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.edvaldotsi.flappybird.MainGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.height = 640;
+        config.width = 480;
+		new LwjglApplication(new MainGame(), config);
+	}
+}
